e1ab3c92b2f3ead44f8e02d3cfc17458f2bc087d
santanam23 <mirmir11.ms@gmail.com>
2022-03-06 22:23:40 -0600

    html created

--- first-app/public/app.js
+++ first-app/public/app.js
@@ -1,34 +1,61 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
-
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
-
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
-
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
-
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
-
-messageForm.addEventListener('submit', e => {
-  e.preventDefault()
-  const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message', message)
-  messageInput.value = ''
-})
-
-function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+function init() {
+    let res_elm = document.createElement("div");
+    res_elm.innerHTML="Hello, How can I help you?" ;
+    res_elm.setAttribute("class","left");
+ 
+    document.getElementById('msg').appendChild(res_elm);
 }
+ 
+ 
+document.getElementById('reply').addEventListener("click", async (e) => {
+    e.preventDefault();
+ 
+    var req = document.getElementById('msg_send').value ;
+ 
+    if (req == undefined || req== "") {
+ 
+    }
+    else{
+     
+        var res = "";
+        await axios.get(`api`).then(data => {
+            res = JSON.stringify(data.data.response)
+        })
+           
+        let data_req = document.createElement('div');
+        let data_res = document.createElement('div');
+ 
+        let container1 = document.createElement('div');
+        let container2 = document.createElement('div');
+ 
+        container1.setAttribute("class","msgCon1");
+        container2.setAttribute("class","msgCon2");
+ 
+        data_req.innerHTML = req ;
+        data_res.innerHTML = res ;
+ 
+ 
+        data_req.setAttribute("class","right");
+        data_res.setAttribute("class","left");
+ 
+        let message = document.getElementById('msg');
+ 
+         
+        message.appendChild(container1);
+        message.appendChild(container2);
+ 
+        container1.appendChild(data_req);
+        container2.appendChild(data_res);
+ 
+        document.getElementById('msg_send').value = "";
+ 
+    function scroll() {
+        var scrollMsg = document.getElementById('msg')
+        scrollMsg.scrollTop = scrollMsg.scrollHeight ;
+    }
+    scroll();
+ 
+    }
+ 
+ 
+    });
